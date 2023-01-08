83af5610169e76f05e4d711b660644de22a059bd
zemownkee <zemownkee@gmail.com>
2023-01-08 17:41:35 -0500

    Format code and remove console logs

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-// placeholder for testing layout and dynamic DOM content functionality
-const testObj = {
-  title: "The Hobbit",
-  author: "J.R.R. Tolkien",
-  pageCount: "1200",
-  isRead: true,
-};
-
 // global document references
 const container = document.querySelector(".card-container");
 const form = document.getElementById("form");
 const submitNew = document.querySelector(".submit");
-const cancelButton = document.querySelector(".cancel");
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".cancel");
 const newBookButton = document.getElementById("new-book");
 const modalDarken = document.querySelector(".darken");
 
 // initial array for library
-let myLibrary = [];
+const myLibrary = [];
 
 function styleButton(button, object) {
   if (object.isRead === true) {
@@ -27,7 +18,6 @@ function styleButton(button, object) {
     button.classList.add("book-unread");
     button.classList.remove("book-read");
   }
-  console.log(button);
 }
 
 function newCard(object) {
@@ -64,6 +54,7 @@ function newCard(object) {
 
 function showCards() {
   myLibrary.forEach((element) => {
+    // eslint-disable-next-line no-param-reassign
     element.order = myLibrary.indexOf(element);
     newCard(element);
   });
@@ -88,17 +79,15 @@ function Book(title, author, pageCount, isRead) {
   this.isRead = isRead;
 }
 
-Book.prototype.toggleRead = function () {
+Book.prototype.toggleRead = function toggleRead() {
   if (this.isRead === true) {
     this.isRead = false;
   } else this.isRead = true;
-  console.log(this);
   refreshLibraryView();
 };
 
-Book.prototype.removeFromLibrary = function () {
+Book.prototype.removeFromLibrary = function removeFromLibrary() {
   myLibrary.splice(this.order, 1);
-  console.log(this);
   refreshLibraryView();
 };
 
@@ -111,7 +100,6 @@ function addBookToLibrary(e) {
   if (!bookInput["is-read"]) {
     cleanedisRead = false;
   } else cleanedisRead = true;
-  console.log(cleanedisRead);
 
   const newObj = new Book(
     bookInput.title,
@@ -119,7 +107,6 @@ function addBookToLibrary(e) {
     bookInput["page-ct"],
     cleanedisRead
   );
-  console.log(newObj);
   myLibrary.push(newObj);
   refreshLibraryView();
 }
